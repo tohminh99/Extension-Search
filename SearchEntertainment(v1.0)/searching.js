3ea752518ed73d95ceba9b23cd03555db389a677
khorjenn <khorjenn1026@gmail.com>
2017-11-19 20:48:44 +0800

    Added add more search

--- SearchEntertainment(v1.0)/searching.js
+++ SearchEntertainment(v1.0)/searching.js
@@ -1,11 +1,23 @@
 document.getElementById("btn_confirm").onclick = function()
 {
 var input = document.getElementById('input_search').value;
+var e = document.getElementById('type').value;
+
+
 function onError(error) {
     console.log(`Error: ${error}`);
   }
-  
+
+if (e==1)
+{
 var createnewtab = browser.tabs.create
 ({url: 'https://www.themoviedb.org/search?query='+input});
 createnewtab.then(onError);
+}
+else
+{
+    var createnewtab = browser.tabs.create
+    ({url: 'https://www.google.com'});
+    createnewtab.then(onError);
+}
 }
